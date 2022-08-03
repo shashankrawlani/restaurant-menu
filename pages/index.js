71f81ebb79617b2c71792ab5c2e2e44794ba77b2
shashankrawlani <shashank@rawlani.com>
2022-08-04 00:42:18 +0530

    fixes errors

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Punjab Sindh Restaurant Menu</title>
-        <meta name="description" content="Punjab Sindh restaurant Menu" />
-        <link
-          rel="icon"
-          href={`${process.env.NEXT_PUBLIC_BASEPATH}/favicon.ico`}
-        />
-      </Head>
       <h1>Punjab Sindh</h1>
     </div>
   );
