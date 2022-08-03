21f975f65248bd642dfc31fea6a59f850e28e992
shashankrawlani <shashank@rawlani.com>
2022-08-04 00:07:31 +0530

    added env & changed h1 css

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,17 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Punjab Sindh</div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Punjab Sindh Restaurant Menu</title>
+        <meta name="description" content="Punjab Sindh restaurant Menu" />
+        <link
+          rel="icon"
+          href={`${process.env.NEXT_PUBLIC_BASEPATH}/favicon.ico`}
+        />
+      </Head>
+      <h1>Punjab Sindh</h1>
+    </div>
+  );
 }
